d4be11bc717a0640291feef9b01ba12f51e646f1
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-07-06 17:12:28 -0600

    REMOVE ALLOW CREDENTIALS

--- app/server.py
+++ app/server.py
@@ -37,7 +37,6 @@ def create_app() -> FastAPI:
     app_.add_middleware(
         CORSMiddleware,
         allow_origins=origins,
-        allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
     )
