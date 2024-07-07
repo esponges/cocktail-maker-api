f67ddc564f5c49e69924e241ae67173a42abca80
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-07-07 09:24:58 -0600

    add ws ping

--- app/server.py
+++ app/server.py
@@ -1,9 +1,10 @@
 # from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from api import router
 from dotenv import load_dotenv
+from asyncio import sleep, create_task
 
 from core.config import config
 
@@ -45,3 +46,38 @@ def create_app() -> FastAPI:
 
 
 app = create_app()
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+    
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+async def send_ping(websocket: WebSocket):
+    while True:
+        try:
+            await websocket.send_text("ping")
+            await sleep(5)  # Send a ping every 5 seconds
+        except Exception:
+            break
+
+manager = ConnectionManager()
+
+@app.websocket("/ws/{session_id}")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    ping_task = create_task(send_ping(websocket))
+    try:
+        while True:
+            await sleep(60)
+    except WebSocketDisconnect:
+        print("disconnected")
+    finally:
+        ping_task.cancel()
+        manager.disconnect(websocket)
+        # await websocket.close()
